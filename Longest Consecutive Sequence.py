f8f27ee944f030973eee2c0284c341f8f0bfb19d
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-07-05 10:01:38 -0700

    Add files via upload

--- Longest Consecutive Sequence.py
+++ Longest Consecutive Sequence.py
@@ -15,3 +15,16 @@ class Solution:
                     length += 1
                 longest = max(length, longest)
         return longest
+
+        # Solution 2
+        if not nums:
+            return 0
+        nums = sorted(list(set(nums)))
+        sofar, output = 1, 1
+        for i in range(1, len(nums)):
+            if nums[i - 1] + 1 == nums[i]:
+                sofar += 1
+            else:
+                sofar = 1
+            output = max(output,sofar)
+        return output
